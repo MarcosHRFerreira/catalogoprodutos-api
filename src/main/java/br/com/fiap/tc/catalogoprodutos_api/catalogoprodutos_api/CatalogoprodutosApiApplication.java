014f81655ca7b04acf5ba2a7e86a2e43c8a905d8
MarcosHRFerreira <marcoshrferreira@gmail.com>
2024-10-30 08:24:37 -0300

    carga inicial

---
+++ src/main/java/br/com/fiap/tc/catalogoprodutos_api/catalogoprodutos_api/CatalogoprodutosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tc.catalogoprodutos_api.catalogoprodutos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoprodutosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoprodutosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/tc/catalogoprodutos_api/catalogoprodutos_api/CatalogoprodutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tc.catalogoprodutos_api.catalogoprodutos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoprodutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
